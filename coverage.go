1f2c1b200a8e680c8dda4b9827f07f397a0ce478
Joshua Kaplan <yhkaplan@gmail.com>
2018-09-01 12:14:58 +0900

    Fix some syntax errors

--- coverage.go
+++ coverage.go
@@ -6,21 +6,23 @@ import (
 )
 
 // Run xcrun xccov view --only-targets --json derivedDataPath
-func getCoverageInfo(derivedDataPath string) (string, err) {
+func getCoverageInfo(derivedDataPath string) (string, error) {
 }
 
 func parseJSON() {}
 
 // Convert"lineCoverage": 0.05829249107521026
-func calculateCoverageStatistics(coverage float) int {}
+func calculateCoverageStatistics(coverage float32) float32 {
+	return coverage * 100
+}
 
 // TODO: Check for DerivedData path envar
 // If not set, assume default
-func getDerivedDataPath(targetName string) (string, err) {
-	envarPath, _ := os.Getenv("DERIVED_DATA_PATH")
-	if envarPath {
-		return envarPath
+func getDerivedDataPath(targetName string) string {
+	envarPath := os.Getenv("DERIVED_DATA_PATH")
+	if envarPath == "" {
+		return fmt.Sprintf("~/Library/Developer/Xcode/DerivedData/%s-*/Build/Products/Logs/Test/*.xccovreport", targetName)
 	}
 
-	return fmt.Sprintf("~/Library/Developer/Xcode/DerivedData/%s-*/Build/Products/Logs/Test/*.xccovreport", targetName)
+	return envarPath
 }
